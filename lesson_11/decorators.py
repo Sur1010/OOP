9ac962416bb33fb6244ed95ed7cd556f822a6887
Sur1020 <sur.stepanyan.20@gmail.com>
2023-05-27 19:21:41 +0400

    lesson_11

--- lesson_11/decorators.py
+++ lesson_11/decorators.py
@@ -1,14 +1,23 @@
+from datetime import datetime
+
+def decorator_for_error(func):
+    def wrapper():
+        try:
+            return func()
+        except Exception as e:
+            with open("log.txt", "a") as file:
+                file.write(f"{e} {datetime.now()}\n")
+
+    return wrapper                
+
 def decorator_for_sensitive_data(size):
     def wrapper(func):
         def wrap():
             long_size = func()
-            if len(long_size) > size:
+            if len(long_size) < size:
                 raise Exception("error")
             return long_size
 
         return wrap
 
     return wrapper            
-
-
-
--- lesson_11/task11.py
+++ lesson_11/task11.py
@@ -1,30 +1,24 @@
-from decorators import decorator_for_sensitive_data
+from decorators import decorator_for_sensitive_data, decorator_for_error
 
+@decorator_for_error
 @decorator_for_sensitive_data(10)
 
 def get_sensitive_data():
     users = []
     with open("passwords.txt","r") as f:
-        for i, j in enumerate(f):
+        for i, line in enumerate(f):
             if i == 0:
-                keys = j.strip().split(',')
+                continue
             else:
-                users.append(j.strip().split(','))
+                users.append(line.strip().split(','))
     list_of_users = []
-    for user in users:
-        users_dict = {}
-        for key , value in enumerate(user):
-            users_dict[keys[key]] = value
+    for name, password  in users:
+        users_dict = {
+            name : password
+        }
         list_of_users.append(users_dict)
 
     
     return list_of_users           
 
 print(get_sensitive_data())
-
-
-
-
-
-
-
